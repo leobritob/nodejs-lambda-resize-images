79d9bd9e10cc5c7ebd08e8e671a00e7c47773282
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-04-21 08:09:40 -0300

    fix: await a promise toBuffer

--- index.js
+++ index.js
@@ -7,25 +7,20 @@ if (process.env.NODE_ENV === 'development') {
 const s3 = new AWS.S3({ apiVersion: '2006-03-01' })
 
 exports.handler = async (event) => {
-  console.log('fuction starting')
-
   const Bucket = event.Records[0].s3.bucket.name
-  const Key = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, ' '))
+  const Key = event.Records[0].s3.object.key
 
   try {
     const { Body } = await s3.getObject({ Bucket, Key }).promise()
 
-    let { width } = await sharp(Body).metadata()
-    width = width < process.env.DEFEAULT_WIDHT ? width : process.env.DEFEAULT_WIDHT
-
-    const newBody = sharp(Body).resize(width).toBuffer()
+    const newBody = await sharp(Body).resize(400).toBuffer()
     const newKey = process.env.OUTPUT_DIRECTORY_S3 + Key.slice(9, Key.length)
 
     await s3.upload({ Bucket, Body: newBody, Key: newKey }).promise()
+
+    await s3.deleteObject({ Bucket, Key }).promise()
   } catch (err) {
     console.log('Error: ' + err)
     throw err
-  } finally {
-    console.log('function finished')
   }
 }
