1d9c33e8a2ef3fbf3dac1031029c0396042851b3
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2021-04-21 08:15:23 -0300

    fix: change a const to environment

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ exports.handler = async (event) => {
   try {
     const { Body } = await s3.getObject({ Bucket, Key }).promise()
 
-    const newBody = await sharp(Body).resize(400).toBuffer()
+    const newBody = await sharp(Body).resize(process.env.DEFEAULT_WIDHT).toBuffer()
     const newKey = process.env.OUTPUT_DIRECTORY_S3 + Key.slice(9, Key.length)
 
     await s3.upload({ Bucket, Body: newBody, Key: newKey }).promise()
